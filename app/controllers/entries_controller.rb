01b8a5a9d52db2db3ea57bb13355e301fbd6b766
Dhruv Chempakasseril <dhruv.chempakasseril@kellogg.northwestern.edu>
2024-03-13 03:46:55 +0000

    save2

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,13 +4,18 @@ class EntriesController < ApplicationController
   end
 
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry["user_id"] = @current_user["id"]
-    @entry.save
+    if @current_user
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry["place_id"] = params["place_id"]
+      @entry.uploaded_image.attach(params["uploaded_image"])
+      @entry["user_id"] = @current_user["id"]
+      @entry.save
+    else
+      flash["notice"] = "Login first."
+    end
     redirect_to "/places/#{@entry["place_id"]}"
   end
 
